273efe48dc356c2ac5ac2fbfcf2238cfed6b6f12
Msfead <Msfead2015@gmail.com>
2023-07-13 08:30:47 -0700

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  *main - Entry point
- *Descraption: A C proogram that points with put function
+ *Descraption: A C proogram that points with put functio
  *Return: Always 0(success)
 */
 int main(void)
