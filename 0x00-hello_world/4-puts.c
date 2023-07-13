c1201b46e71244daed447562c4d7a3e65c91557c
Msfead <Msfead2015@gmail.com>
2023-07-13 07:47:48 -0700

    my first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * descraption: A C program that prints with put funcation
+ *
+ * Return: always 0 (ucsess)
+*/
+
+	int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
