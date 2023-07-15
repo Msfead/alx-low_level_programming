b9c635f57e94630af75d6dfeb9dab0d13c311684
Msfead <Msfead2015@gmail.com>
2023-07-14 18:58:31 -0700

    c varibles

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Descraption: prints all possible different combinations of two digits.
+ * Return: Always 0
+*/
+int main(void)
+{
+	int n = 0;
+	int m, l;
+
+while (n <= 9)
+{
+m = 0;
+while (m <= 9)
+{
+l = 0;
+while (l <= 9)
+{
+if (n != m &&
+n < m &&
+m != l &&
+m < l)
+{
+putchar(n + 48);
+putchar(m + 48);
+putchar(l + 48);
+
+if (n + m + l != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+l++;
+}
+m++;
+}
+n++;
+}
+putchar('\n');
+
+return (0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *Descraption: prints all possible combinations of two two-digit numbers.
+ *Retutn: Always 0
+*/
+int main(void)
+
