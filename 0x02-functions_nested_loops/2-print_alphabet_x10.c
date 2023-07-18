d1e8859d3820a40d98e8aa884371b57118b33ff5
Msfead <Msfead2015@gmail.com>
2023-07-18 16:21:30 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void);
 {
 int i, j;
 
-for (j = 1; i <= 10; i++)
+for (j = 0; i <= 9; i++)
 {
-	for (j = 97; j <= 122; j++)
+	for (j = 'a'; j <= 'z'; j++)
 		_putchar(j);
 	_putchar('\n');
 }
