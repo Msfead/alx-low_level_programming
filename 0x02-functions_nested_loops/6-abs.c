9de806b46f138275f8ea9ef391014c52eda983b3
Msfead <Msfead2015@gmail.com>
2023-07-18 07:41:26 -0700

    C Functions nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - Entry point
+*Descraption:computes the absolute value of an integer.
+*Return: Always 0
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
