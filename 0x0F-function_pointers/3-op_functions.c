46a19d9642863292961e159efe4b79860c25f3df
Msfead <Msfead2015@gmail.com>
2023-08-16 04:27:40 -0700

    M

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two
+ * @a: first
+ * @b: seconed
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two
+ * @a : first
+ * @b: seconed
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multipluy  two
+ * @a : first
+ * @b: seconed
+ * Return: difference
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div  - divid  two
+ * @a : first
+ * @b: seconed
+ * Return: difference
+ */
+
+int op_div(int a, int b)
+{
+	return ( a / b);
+}
+
+/**
+ * op_mod  - moudle  two
+ * @a : first
+ * @b: seconed
+ * Return: difference
+ */
+
+int op_mod(int a, int b)
+{
+	return ( a % b);
+}
