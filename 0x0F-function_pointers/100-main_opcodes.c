e1f85f873adc16639ebbfbc7e39c3502544a4a76
Msfead <Msfead2015@gmail.com>
2023-08-16 04:54:57 -0700

    M

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check code for school student
+ * @argc: number
+ * @argv: vector
+ * Return: 0 always
+ */
+
+int main(int argc, char **argv)
+{
+
+char *p = (char *)main;
+int b;
+
+if (argc != 2)
+	printf("ERROR\n"), exit(1);
+b = atoi(argv[1]);
+if (b < 0)
+	printf("ERROR\n"), exit(2);
+
+while (b--)
+	printf("%02hhx%s", *p++, b ? " " : "\n");
+return (0);
+}
